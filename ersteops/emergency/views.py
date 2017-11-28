75b67ed227781d67d02b87322a07538e4fa44370
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-11-28 10:18:34 -0600

    add minimal view

--- ersteops/emergency/views.py
+++ ersteops/emergency/views.py
@@ -4,3 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.views.generic import View, CreateView, ListView
+
+class EmergencyRecord(View):
+	template_name= "emergency/blank.html"
+	def get(self, request, *args, **kwargs):
+		form=''
+		return render(request, self.template_name,{"form": form})
