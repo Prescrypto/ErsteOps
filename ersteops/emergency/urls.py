dfe0ef7ee9b4f231d57c9edb8a1aa5725ebd63b7
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-12-06 16:13:00 -0600

    add update emergency endpoint

--- ersteops/emergency/urls.py
+++ ersteops/emergency/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from . import views
-from emergency.views import EmergencyBlank, EmergencyNew, EmergencyListView, EmergencyDetailView, EmergencyDashbordList, EmergencyDerivation
+from emergency.views import EmergencyBlank, EmergencyNew, EmergencyListView, EmergencyDetailView, EmergencyDashbordList, EmergencyDerivation, EmergencyUpdate
 
 urlpatterns = [
     url(r'^$',EmergencyBlank.as_view(),name="emergencyblank"),
     url(r'^new/$',EmergencyNew.as_view(),name="emergencynew"),
     url(r'^list/$',EmergencyListView.as_view(),name="emergencylist"),
+    url(r'^update/(?P<pk>[0-9]+)/$',EmergencyUpdate.as_view(),name="emergencyupdate"),
     url(r'^dashbord/$',EmergencyDashbordList.as_view(),name="emergencydashbord"),
     url(r'^derivation/$',EmergencyDerivation.as_view(),name="emergencyderivation"),
     url(r'^(?P<pk>[0-9]+)/$', EmergencyDetailView.as_view(), name='emergencydetail'),
