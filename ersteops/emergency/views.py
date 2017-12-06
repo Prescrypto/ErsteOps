d731d9a86b3ee5877b0283f48acad352027922d8
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-12-06 16:13:27 -0600

    add emergency update view

--- ersteops/emergency/views.py
+++ ersteops/emergency/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.views.generic import View, CreateView, ListView, DetailView
+from django.views.generic import View, CreateView, ListView, DetailView, UpdateView
 from emergency.models import Emergency,AttentionDerivation
 from django.utils import timezone
 
@@ -97,3 +97,45 @@ class EmergencyDerivation(CreateView):
                 'notes',
         ]
     success_url = '/emergency/list/'
+
+class EmergencyUpdate(UpdateView):
+    template_name = "emergency/update.html"
+    model = Emergency
+    fields = ['id','odoo_client',
+                'grade_type',
+                'zone',
+                'start_time',
+                'end_time',
+                'is_active',
+                'unit',
+                'unit_assigned_time',
+                'unit_dispatched_time',
+                'arrival_time',
+                'attention_time',
+                'derivation_time',
+                'hospital_arrival',
+                'patient_arrival',
+                'final_emergency_time',
+                'address_street',
+                'address_extra',
+                'address_zip_code',
+                'address_county',
+                'address_col',
+                'address_between',
+                'address_and_street',
+                'address_ref',
+                'address_front',
+                'address_instructions',
+                'address_notes',
+                'caller_name',
+                'caller_relation',
+                'patient_allergies',
+                'patient_illnesses',
+                'patient_notes',
+                'main_complaint',
+                'complaint_descriprion',
+                'required_attention',
+                'subscription_type'
+                ]
+    success_url = '/emergency/list/'
+
