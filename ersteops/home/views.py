c205a861f378c32ac3b57e5630febd2136d50eb3
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2017-12-09 15:40:45 -0600

    add login logic and redirect if has registered

--- ersteops/home/views.py
+++ ersteops/home/views.py
@@ -1,10 +1,28 @@
+import logging
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 
 
 def home(request):
     ''' Login page '''
-    print("HI")
-    return render(request, 'home/login.html')
+    logger = logging.getLogger('django_info')
+    login_template = "home/login.html"
+    if not request.user.is_anonymous():
+        return redirect("emergencyblank")
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            logger.info("Success login auth to: {}".format(user.username))
+            login(request, user)
+            return redirect('emergencyblank')
+        else:
+            logger.info("Error on login auth to: {}".format(user.username))
+            return render(request, login_template, {"auth_error": "Tu email o password no coinciden, intenta de nuevo"})
+
+    return render(request, login_template)
 
 def logout_page(request):
     ''' logout url '''
