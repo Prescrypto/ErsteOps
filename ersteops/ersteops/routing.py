569d182c0f7532758e7003c77f5bbe023993e014
vash512 <phyrox.vash512@gmail.com>
2017-12-13 09:56:23 -0600

    clean minichat example

--- ersteops/ersteops/routing.py
+++ ersteops/ersteops/routing.py
@@ -1,7 +1,6 @@
 from channels import include
 
 channel_routing = [
-    #include("minichat.routing.websocket_routing", path=r"^/chat/stream"),
+    #include("notifications.routing.websocket_routing", path=r"^/notification/dashboard"),
     include("notifications.routing.websocket_routing"),
-    #include("minichat.routing.custom_routing")
 ]
--- ersteops/minichat/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from .models import Room
-
-
-def chat_room(request, label):
-    room, created = Room.objects.get_or_create(label=label)
-    messages = reversed(room.messages.order_by('-timestamp')[:50])
-    ctx={'room': room, 'messages': messages}
-    return render(request, "chat/room.html", ctx)
