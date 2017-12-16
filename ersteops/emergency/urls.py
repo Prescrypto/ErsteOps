3c2783df408ce80cc7d017320cdc028f2a9759c4
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2017-12-15 21:04:11 -0600

    add urls to reactivate and end emergency

--- ersteops/emergency/urls.py
+++ ersteops/emergency/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from emergency.views import EmergencyBlank, EmergencyNew, EmergencyListView, EmergencyDetailView, EmergencyDashbordList, EmergencyDerivation, EmergencyUpdate, EmergencyClientOdoo, EmergencyClientModal,EmergencyNewModal, EmergencyGetPatient, OdooSubscription
+from emergency.views import EmergencyBlank, EmergencyNew, EmergencyListView, EmergencyDetailView, EmergencyDashbordList, EmergencyDerivation, EmergencyUpdate, EmergencyClientOdoo, EmergencyClientModal,EmergencyNewModal, EmergencyGetPatient, OdooSubscription, EmergencyActivate, EmergencyEnd
 
 urlpatterns = [
     url(r'^$',EmergencyBlank.as_view(),name="emergencyblank"),
@@ -14,5 +14,7 @@ urlpatterns = [
     url(r'^dashboard/$',EmergencyDashbordList.as_view(),name="emergencydashbord"),
     url(r'^derivation/$',EmergencyDerivation.as_view(),name="emergencyderivation"),
     url(r'^(?P<pk>[0-9]+)/$', EmergencyDetailView.as_view(), name='emergencydetail'),
+    url(r'^activate/(?P<patient_id>\d+)/$', EmergencyActivate.as_view(), name='emergencyactivate'),
+    url(r'^end/(?P<patient_id>\d+)/$', EmergencyEnd.as_view(), name='emergencend'),
     url(r'^getpatient/(?P<patient_id>\d+)/$', EmergencyGetPatient.as_view(), name='emergencypatient'),
 ]
