5a73d469a482e7f7a417c5693b1850ae3410c31c
Carlos Rosquillas <crosquillas@gmail.com>
2017-12-30 15:25:58 -0800

    Add autocomplete component to modal
    - Add Vue component file support
    - Separate DOM rendering from component definitions
    - Create dashboard log component
    - Replace inline dashboard log with new component
    - Render component as child of modal
    - Add VueX state management
    - Add network request constants
    - Add mutations and basic action
    - Map input to local state
    - Trigger action from component
    - Add library to handle network requests
    - Configure base http instance
    - Search term from local state from backend
    - Render search results in frontend
    - Indicate no results returned
    - Map loading from store to local state
    - Add loading indicator to autocomplete component
    - Style autocomplete input
    - Style submit button
    - Add basic form validation
    - Show validation errors on input
    - Disable submit when form is invalid
    - Style validation errors
    - Style autocomplete list
    
    Fixes F1ST-499

---
+++ frontend/components/Search/script.js
@@ -0,0 +1,48 @@
+import { mapState, mapActions, mapGetters } from 'vuex';
+import Loader from 'vue-spinner/src/ScaleLoader.vue';
+
+export default {
+  name: 'search',
+
+  components: { Loader },
+
+  data() {
+    // query is for the input
+    // pristine indicates there's no prior search
+    return {
+      query: '',
+      pristine: true,
+    };
+  },
+
+  computed: {
+    invalid() {
+      return this.fields.term.invalid;
+    },
+
+    // loading is for hiding the spinner
+    // suggestions is the server results array
+    ...mapState({
+      loading: 'loading',
+      suggestions: 'suggestions',
+    }),
+    // memoized computed property that indicates whether there are suggestions
+    ...mapGetters(['hasSuggestions']),
+  },
+
+  methods: {
+    // on submit, disable the default <form> submit, indicate the form is
+    // dirty, and run the search action with the input value
+    submit() {
+      this.$validator.validateAll().then(valid => {
+        if (valid) {
+          this.pristine = false;
+          this.search(this.query);
+        }
+      });
+    },
+
+    // maps the search action from the store to the component
+    ...mapActions(['search']),
+  },
+};
