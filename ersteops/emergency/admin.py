f214a2831129f2d4ef54ed83abbbc80fbbe27010
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-01-22 15:14:42 -0600

    add is active action on admin panel

--- ersteops/emergency/admin.py
+++ ersteops/emergency/admin.py
@@ -7,11 +7,26 @@ from django.contrib import admin
 from .models import (Emergency, AttentionKind, AttentionZone, AttentionHospital,
 	AttentionDerivation, ServiceCategory)
 
+def update_is_active(EmergencyAdmin, request, queryset):
+    # action fuction to help change to plan to users
+    queryset.update(is_active=True)
+
+update_is_active.short_description = "Cambiar Emergencias a estado Activo"
+
+def update_non_active(EmergencyAdmin, request, queryset):
+    # action fuction to help change to plan to users
+    queryset.update(is_active=False)
+
+update_non_active.short_description = "Cambiar Emergencias a estado Inactivo"
+
 
 class EmergencyAdmin(admin.ModelAdmin):
     ''' Custom Emergency Admin Panel '''
-    list_display = ("id", "odoo_client", "grade_type", "zone", "created_at", "is_active", )
+    list_display = ("id", "odoo_client","patient_name", "grade_type", "zone", "created_at", "is_active", )
     list_filter = ("id", "odoo_client", "grade_type", "zone", "is_active", )
+    search_fields = ('odoo_client', 'patient_name', )
+
+    actions = [update_is_active, update_non_active]
 
 
 admin.site.register(Emergency, EmergencyAdmin)
