5fa1ec54d251796c2a52cdc682634604c759e80b
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-02-08 13:15:01 -0600

    delete vehicle app

--- ersteops/vehicle/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Unit, Brand, UnitType
-
-admin.site.register(Unit)
-admin.site.register(Brand)
-admin.site.register(UnitType)
