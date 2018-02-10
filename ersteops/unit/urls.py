987b7d13e00e4d31240a0f5bdee7c0bb36e02338
Jesús Alvarado Garzón <xtornasol512@gmail.com>
2018-02-10 12:44:31 -0600

    add unit json detail view and add required ajax request

--- ersteops/unit/urls.py
+++ ersteops/unit/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 
-from .views import unit_json_list, alliance_unit_json_list
+from .views import unit_json_list, alliance_unit_json_list, detail_unit_json
 
 urlpatterns = [
     # Ajax Views
+    url(r'^ajax/detail/(?P<id_unit>[0-9]+)$', detail_unit_json, name="detail_unit_json"),
     url(r'^ajax/local/$', unit_json_list, name="local_units_json_list"),
     url(r'^ajax/alliance/$', alliance_unit_json_list, name="alliance_units_json_list"),
 ]
--- ersteops/unit/views.py
+++ ersteops/unit/views.py
@@ -1,16 +1,39 @@
+import json
+from django.shortcuts import get_object_or_404
 from django.core import serializers
 from django.http import HttpResponse
 from .models import Unit
 from .utils import UNIT_LIST_FIELD
 
+BAD_REQUEST = HttpResponse(json.dumps({'error': 'Bad Request'}), status=400, content_type='application/json')
+
 def unit_json_list(request):
     ''' List Json View for local available units '''
-    units = Unit.objects.available_units()
-    data = serializers.serialize('json', list(units), fields=UNIT_LIST_FIELD)
-    return HttpResponse(data, content_type='application/json')
+    if request.is_ajax():
+        units = Unit.objects.available_units()
+        data = serializers.serialize('json', list(units), fields=UNIT_LIST_FIELD)
+        return HttpResponse(data, content_type='application/json', status=200)
+    else:
+        return BAD_REQUEST
+
+def detail_unit_json(request, id_unit):
+    ''' Detail view of unit '''
+    if request.is_ajax():
+        unit = Unit.objects.filter(pk=id_unit)
+
+        if len(unit) == 0:
+            return HttpResponse(json.dumps({'error': 'Unidad no encontrada'}), status=404, content_type='application/json')
+
+        data = serializers.serialize('json', unit, fields=UNIT_LIST_FIELD)
+        return HttpResponse(data, content_type='application/json', status=200)
+    else:
+        return BAD_REQUEST
 
 def alliance_unit_json_list(request):
     ''' List Json View for alliance available units '''
-    units = Unit.objects.available_alliance_units()
-    data = serializers.serialize('json', list(units), fields=UNIT_LIST_FIELD)
-    return HttpResponse(data, content_type='application/json')
+    if request.is_ajax():
+        units = Unit.objects.available_alliance_units()
+        data = serializers.serialize('json', list(units), fields=UNIT_LIST_FIELD)
+        return HttpResponse(data, content_type='application/json', status=200)
+    else:
+        return BAD_REQUEST
