12db8a5f25fd97d0aaaaa012d495e64b34665850
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-02-12 20:01:41 -0600

    add basic view

--- ersteops/reports/views.py
+++ ersteops/reports/views.py
@@ -1,3 +1,12 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+class BaseReport(View):
+    template_name = "reports/basereport.html"
+    def get(self, request, *args, **kwargs):
+        #form = OdooClientForm
+
+        return render(request, self.template_name,{"form": ""})
