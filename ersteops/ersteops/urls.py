384ef3ac5810488e9ff5d544a0f0f03634d08fe8
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-05-02 18:43:56 -0500

    add end points for decision tree

--- ersteops/ersteops/urls.py
+++ ersteops/ersteops/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
-from ajaxapi.views import get_subscriptor
+from ajaxapi.views import get_subscriptor,get_symptom_zero
 
 urlpatterns = [
 
@@ -15,7 +15,10 @@ urlpatterns = [
     url(r'^units/', include('unit.urls')),
     url(r'^notify/', include('notifications.urls')),
     url(r'^ajaxapi/getsubscriptor/',get_subscriptor, name="get_subscriptor"),
+    url(r'^ajaxapi/get_symptom/',get_symptom_zero, name="get_symptom"),
+    url(r'^decisiontree/',include('decisiontree.urls')),
     # Jet Dashboard
     url(r'^jet/', include('jet.urls','jet')),
     url(r'^jet/dashboard/', include('jet.dashboard.urls','jet-dashboard')),
+
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
