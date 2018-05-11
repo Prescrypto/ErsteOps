de48bf9e0480187c709d178b2903bfd99d3419b5
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-05-11 16:07:34 -0500

    add end point

--- ersteops/ersteops/urls.py
+++ ersteops/ersteops/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 from django.conf import settings
 from django.conf.urls.static import static
-from ajaxapi.views import get_subscriptor,get_symptom_zero
+from ajaxapi.views import get_subscriptor,get_symptom_zero, get_emergency_grade
 
 urlpatterns = [
 
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^notify/', include('notifications.urls')),
     url(r'^ajaxapi/getsubscriptor/',get_subscriptor, name="get_subscriptor"),
     url(r'^ajaxapi/get_symptom/',get_symptom_zero, name="get_symptom"),
+    url(r'^ajaxapi/get_emergency_grade/',get_emergency_grade, name="get_emergency_grade"),
     url(r'^decisiontree/',include('decisiontree.urls')),
     # Jet Dashboard
     url(r'^jet/', include('jet.urls','jet')),
