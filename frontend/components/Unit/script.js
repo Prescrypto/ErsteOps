ae4b3243529c829d1d6523d93a9ec92410b947a9
Carlos Rosquillas <crosquillas@gmail.com>
2018-05-17 01:01:43 -0700

    Fully unmount and remount unit popover content

--- frontend/components/Unit/script.js
+++ frontend/components/Unit/script.js
@@ -3,9 +3,20 @@ import UnitDetails from 'components/UnitDetails';
 export default {
   name: 'unit',
   components: { UnitDetails },
+  data: () => ({
+    hover: false,
+  }),
   computed: {
     label() {
       return this.data.identifier;
+    },
+  },
+  methods: {
+    handleShow() {
+      this.hover = true;
+    },
+    handleHide() {
+      this.hover = false;
     },
   },
   props: {
