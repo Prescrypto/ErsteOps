df660821813a301081bf10284e8becd64f58cb0c
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-05-22 12:03:05 -0500

    add endpoint to vue select test

--- ersteops/decisiontree/urls.py
+++ ersteops/decisiontree/urls.py
@@ -3,11 +3,12 @@ from django.conf.urls import url
 from django.contrib.auth.decorators import login_required
 from . import views
 #from reports.views import Dashbord
-from .views import UploadSymptomData, ProcessUploadSymptomData,SearchSymptomData
+from .views import UploadSymptomData, ProcessUploadSymptomData,SearchSymptomData, SearchSymptomData2
 #from reports.views import ReportBioData, EmployeeReport
 
 urlpatterns = [
     url(r'^upload/$', login_required(UploadSymptomData.as_view(), login_url='/login/'), name="UploadUberData"),
-    url(r'^searchsymptom/$', login_required(SearchSymptomData.as_view(), login_url='/login/'), name="searchsymptom"),    
+    url(r'^searchsymptom/$', login_required(SearchSymptomData.as_view(), login_url='/login/'), name="searchsymptom"),
+    url(r'^searchsymptom2/$', login_required(SearchSymptomData2.as_view(), login_url='/login/'), name="searchsymptom2"),
     url(r'^upload/(?P<data_id>\d+)/$', login_required(ProcessUploadSymptomData.as_view(), login_url='/login/'), name="ProcessUploadUberData"),
 ]
