b71ae9aa1ba55efbbf0349452e10f8b2bd8acf55
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-11-20 23:29:05 -0600

    add datetime registration

--- frontend/components/Timers/script.js
+++ frontend/components/Timers/script.js
@@ -1,9 +1,20 @@
-// import { mapState, mapMutations } from 'vuex';
+import { mapState, mapMutations } from 'vuex';
 import Loader from 'vue-spinner/src/ScaleLoader.vue';
-// import { UPDATE_COPAGO_AMOUNT } from 'store/constants';
+import { UPDATE_COPAGO_AMOUNT } from 'store/constants';
+import { Datetime } from 'vue-datetime';
+import 'vue-datetime/dist/vue-datetime.css';
 
 export default {
   name: 'timers',
   inject: ['$validator'],
-  components: { Loader },
+  components: { Loader, Datetime },
+  computed: {
+    source() {
+      return this.emergency;
+    },
+  },
+  ...mapState(['loading', 'emergency']),
+  methods: {
+    ...mapMutations({ updateCopago: UPDATE_COPAGO_AMOUNT }),
+  },
 };
