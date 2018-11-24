6c0716aee4bbac40905fc752b286e1ca534046ca
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2018-11-23 23:11:50 -0600

    add dashboard timers basic files

---
+++ frontend/components/DashboardTimers/script.js
@@ -0,0 +1,19 @@
+// import { mapActions } from 'vuex';
+
+// export default {
+//   name: 'dashboard-timers',
+//   props: ['emergency'],
+//   data: () => ({
+//    	picked: null,
+//   }),
+//   // $_veeValidate: {
+//   //  validator: 'new',
+
+//   // },
+//   methods: {
+//   	  show_timers(){
+//   			return true;
+//   		},
+// 	},
+
+// }
