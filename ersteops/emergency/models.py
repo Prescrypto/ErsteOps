ebf580c2fd050e5225f3984f631a6d79fddde539
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2019-06-11 19:15:42 -0500

    add phone fields to emergency

--- ersteops/emergency/models.py
+++ ersteops/emergency/models.py
@@ -110,6 +110,9 @@ class Emergency(models.Model):
     subscription_type = models.CharField('Subscripción', max_length=100, default='', blank=True)
     copago_amount = models.IntegerField("Monto de Copago", blank=True, null=True, default=0)
 
+    #Telephones
+    tel_local = models.CharField('Tel de contacto',max_length=33,default='',blank=True)
+    tel_mobile = models.CharField('Movil de contacto',max_length=33,default='',blank=True)
     # TODO when create derivation
     # derivation = models.ManyToManyField('AttentionDerivation',
     #     related_name = 'derivation_issue',
@@ -272,6 +275,8 @@ def emergency_dictionary(instance):
         "main_complaint":instance.main_complaint,
         "complaint_description":instance.complaint_description,
         "subscription_type":instance.subscription_type,
+        "tel_local":instance.tel_local,
+        "tel_mobile":instance.tel_mobile,
     }
 
     return emergDict
