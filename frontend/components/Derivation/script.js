825d25b1da497a2974fa53ace5363fb4a29f3f26
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2019-08-13 22:27:54 -0500

    add minimal FE derivation code

--- frontend/components/Derivation/script.js
+++ frontend/components/Derivation/script.js
@@ -1,1 +1,20 @@
-// import { mapState, mapMutations } from 'vuex';
+import { mapState } from 'vuex';
+import Loader from 'vue-spinner/src/ScaleLoader.vue';
+
+export default {
+  name: 'emergency-derivation',
+  props: ['emergency'],
+  components: { Loader },
+  computed: {
+    ...mapState(['loading', 'emergency']),
+    source() {
+      return this.emergency;
+    },
+    emergency_link() {
+      const eName = `/admin/emergency/emergency/${
+        this.emergency.id
+      }/change/#/tab/module_5/`;
+      return eName;
+    },
+  },
+};
