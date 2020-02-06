2165fadbe0260c2c5de055120644068c204f5b5d
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2020-02-06 15:23:32 -0600

    add mapHopitals to derivations

--- frontend/components/Derivation/script.js
+++ frontend/components/Derivation/script.js
@@ -4,17 +4,24 @@ import Loader from 'vue-spinner/src/ScaleLoader.vue';
 export default {
   name: 'emergency-derivation',
   props: ['emergency'],
+
+  data: () => ({
+    // hospitals: [],
+    // enableRadio: false,
+  }),
+
   components: { Loader },
+  mounted() {
+    // this.getHospitals();
+  },
   computed: {
-    ...mapState(['loading', 'emergency']),
-    // ...mapGetters(['actualDerivations']),
-    // source() {
-    //  return this.emergency;
-    // },
+    // mapHospitas is defined in /utils/preload.js
+    ...mapState(['loading', 'emergency', 'mapHospitals']),
+
     emergency_link() {
       const eName = `/admin/emergency/emergency/${this.emergency.id}/change/#/tab/module_5/`;
-      console.log(this.emergency);
       return eName;
     },
   },
+  methods: {},
 };
