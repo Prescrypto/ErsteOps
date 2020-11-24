a481ba12531d72216fc7ee2e89bd197f77bd5d36
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2020-11-24 15:31:18 -0600

    upgrade node version

--- bin/setup_box.sh
+++ bin/setup_box.sh
@@ -38,8 +38,11 @@ sudo pip3 install -vU setuptools
 
 echo "nodejs"
 sudo apt-get install -y curl
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+# sudo apt-get install -y nodejs
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
+
 
 echo "yarn"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E074D16EB6FF4DE3
