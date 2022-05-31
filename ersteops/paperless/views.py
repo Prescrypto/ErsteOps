a80864a0f26e77b6227c2303285ac156d94c45bc
vezino <juanmanuelriverom@gmail.com>
2022-05-31 15:48:40 -0500

    add geo api variable to map request

--- ersteops/paperless/views.py
+++ ersteops/paperless/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import View, CreateView, ListView
 from .models import MedicalReport
+from django.utils import timezone
 # Create your views here.
 
-class MedicalReportNew(CreateView):
-    template_name = "paperless/paperless_new.html"
+
+
+class MedicalReportNew(ListView):
+    template_name = "paperless/paperless_new_medical_report.html"
     model = MedicalReport
 
 
@@ -13,5 +16,10 @@ class MedicalReportNew(CreateView):
 
 class MedicalReportList(ListView):
     #template_name = "paperless/paperless_list.html"
-    template_name = "paperless/paperless_map.html"
+    template_name = "paperless/paperless_list_medical_reports.html"
     model = MedicalReport
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
