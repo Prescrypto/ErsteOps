16602fc306e84839befa700fcbdf0352530dd5f1
vezino <juanmanuelriverom@gmail.com>
2022-06-21 15:55:12 -0500

    fix endpoints

--- ersteops/paperless/urls.py
+++ ersteops/paperless/urls.py
@@ -1,8 +1,18 @@
-from django.conf.urls import url
-from .views import  MedicalReportNew, MedicalReportList
+#from django.conf.urls import url, path 
+from django.urls import include, path
+from paperless.views import  MedicalReportNew, MedicalReportList, MedicalReportDetail
+
+# urlpatterns = [
+#     url(r'^new/(?P<pk>[0-9]+)/$',MedicalReportNew.as_view(), name="medicalreportnew"),
+#     url(r'^new/$',MedicalReportNew.as_view(), name="medicalreportnew"),
+#     url(r'^',MedicalReportList.as_view(), name="medicalreportlist"),
+#     url(r'^detail/(?P<pk>[0-9]+)/$',MedicalReportDetail.as_view(), name="medical_report_detail"),
+# ]
+
 
 urlpatterns = [
-    url(r'^new/$',MedicalReportNew.as_view(), name="medicalreportnew"),
-    url(r'^',MedicalReportList.as_view(), name="medicalreportlist"),
-
+    path('new/<int:pk>/',MedicalReportNew.as_view(), name="medicalreportnew"),
+    path('new/',MedicalReportNew.as_view(), name="medicalreportnew"),
+    path('',MedicalReportList.as_view(), name="medicalreportlist"),
+    path('detail/<int:pk>/',MedicalReportDetail.as_view(), name="medical_report_detail"),
 ]
