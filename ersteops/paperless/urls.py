dc4deffc990d78c62bc873ccfad3279b89541dc9
vezino <juanmanuelriverom@gmail.com>
2022-07-07 21:06:09 -0500

    add ajax endpoint

--- ersteops/paperless/urls.py
+++ ersteops/paperless/urls.py
@@ -1,6 +1,6 @@
 #from django.conf.urls import url, path 
 from django.urls import include, path
-from paperless.views import  MedicalReportNew, MedicalReportList, MedicalReportDetail, MedicalReportActive
+from paperless.views import  MedicalReportNew, MedicalReportList, MedicalReportDetail, MedicalReportActive, new_medicalreport
 
 # urlpatterns = [
 #     url(r'^new/(?P<pk>[0-9]+)/$',MedicalReportNew.as_view(), name="medicalreportnew"),
@@ -15,5 +15,6 @@ urlpatterns = [
     path('new/',MedicalReportNew.as_view(), name="medicalreportnew"),
     path('',MedicalReportList.as_view(), name="medicalreportlist"),
     path('detail/<int:pk>/',MedicalReportDetail.as_view(), name="medical_report_detail"),
-    path('active/',MedicalReportActive.as_view(), name= "medical_report_active"),
+    path('active/', MedicalReportActive.as_view(), name= "medical_report_active"),
+    path('ajax/new/', new_medicalreport, name= "ajax_new_medical_report"),
 ]
