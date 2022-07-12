a15dd44e81f2cdf5af2d0ec3728610ad787d99f5
vezino <juanmanuelriverom@gmail.com>
2022-07-12 14:31:54 -0500

    update model

--- ersteops/paperless/models.py
+++ ersteops/paperless/models.py
@@ -95,6 +95,8 @@ class MedicalReport(models.Model):
     service_unit = models.CharField("Unidad Movil",max_length=10,default='')
     service_unit_type = models.CharField("Tipo Unidad",max_length=50,default='')
     service_unit_plate = models.CharField("No.Placas",max_length=10,default='')
+    service_crum = models.CharField("Folio CRUM",max_length=20,default='', blank=True)
+    service_crum_dr = models.CharField("Medico CRUM",max_length=60,default='', blank=True)
     # Units m2m relation
     # Paient Data
     service_geo_lat = models.CharField('Latitud',max_length=15, blank=True ,default='')
@@ -102,26 +104,25 @@ class MedicalReport(models.Model):
     patient_name = models.CharField('Nombre del Paciente', max_length=255, default='')
     patient_gender = models.CharField('genero', max_length=9, default= '', blank=True, choices=GENDER)
     patient_age = models.IntegerField('edad (años)', default=0, blank=True)
+    patient_address = models.CharField('Direccion', max_length=50, default='', blank=False)
     patient_affiliations = models.CharField("Filiacion",max_length=50,blank=True)
     patient_unknow = models.CharField("Paciente Desconocido",max_length=50,blank=True)
     patient_clothes = models.CharField("Vestimenta", max_length=50,blank=True)
     patient_phone = models.CharField("Telefono:", max_length=15,blank=True ,default='')
-    attention_place = models.CharField('Sitio de Atencion', max_length=9, default= '', blank=True, choices=ATENTION_PLACE)
+    attention_place = models.CharField('Sitio de Atencion', max_length=20, default= '', blank=True, choices=ATENTION_PLACE)
+    other_attention_place = models.CharField('Otro Sitio de Atencion', max_length=20, default= '', blank=True, choices=ATENTION_PLACE)
     skin_color = models.CharField("Coloración de piel", max_length=50, default= '', blank=True,)
     service_type = models.CharField('Tipo de Servicio', max_length=9, default= '', blank=True, choices=SERVICE_TYPE)
     consultation_reason = models.CharField('Motivo de la Consulta', max_length=20, default= '', blank=True, choices=CONSULTATION_REASON)
+    other_consultation_reason = models.CharField('Otro Motivo de la Consulta', max_length=20, default= '', blank=True)
     event_type = models.CharField('Tipo de Evento', max_length=20, default= '', blank=True, choices=EVENT_TYPE)
-    traumatics = models.CharField('Traumatico', max_length=20, default= '', blank=True, choices=TRAUMATICS)
+    traumatics = models.CharField('Traumatico', max_length=100, default= '', blank=True, choices=TRAUMATICS)
+    other_traumatics = models.CharField('Otro Traumatico', max_length=50, default= '', blank=True, choices=TRAUMATICS)
     airway = models.CharField('Via Aerea', max_length=20, default= '', blank=True, choices=AIRWAY)
+    other_airway = models.CharField('Otra Via Aerea', max_length=20, default= '', blank=True, choices=AIRWAY)
     # (physical_exploration = pe)
-    pe_time = models.DateTimeField("Dia/Hora",blank= True, null =True)
-    pe_heart_rate = models.CharField("Frequencia Cardiaca", max_length=20,default='', blank=True)
-    pe_respiratory_rate = models.CharField("Frequencia Respiratoria", max_length=20,default='', blank=True)
-    pe_blood_pressure = models.CharField("Presion Arterial", max_length=20,default='', blank=True)
-    pe_temperature = models.CharField("Temperature", max_length=20,default='', blank=True)
-    pe_oxygen_saturation = models.CharField("Saturacion O2", max_length=20,default='', blank=True)
-    pe_glucometry = models.CharField("Glucometria", max_length=20,default='', blank=True)
-    pe_glassgow = models.CharField("Glassgow", max_length=20,default='', blank=True)
+    physical_exploration = models.ManyToManyField('PhysicalExploration', 
+        related_name='physical_exploration', blank=True)
     # Normal
     normal_head = models.CharField("Cabeza", blank=True, default='', max_length=10, choices=YES_NO)
     normal_face = models.CharField("Face", blank=True, default='', max_length=10, choices=YES_NO)
@@ -133,7 +134,10 @@ class MedicalReport(models.Model):
 
     current_condition = models.TextField("Padecimiento Actual",blank=True)
 
-    pupil_state = models.CharField("Estado de las Pupilas",max_length=20, default='', blank=True ,choices=PUPIL_STATE)
+    pupil_state_left = models.CharField("Estado de la pupila, Izquierda",max_length=50, default='', blank=True ,choices=PUPIL_STATE)
+    pupil_state_right = models.CharField("Estado de la pupilas, Derecha",max_length=50, default='', blank=True ,choices=PUPIL_STATE)
+    #inmovilization = models.
+    #inmovilization_type 
 
     pathological_history_daibetes_melitus = models.CharField("Diabetes Melitus",blank= True, default=False, max_length=10, choices=YES_NO )
     pathological_history_arterial_hypertension = models.CharField("Hipertension Arterial",blank= True, default=False , max_length=10, choices=YES_NO)
@@ -164,4 +168,30 @@ class MedicalReport(models.Model):
     # Returns a verbose name - adjusted for Python3
     def __str__(self):
         return "{}, {}, {}, {}".format(unicodedata.normalize('NFKD',str(self.id)),unicodedata.normalize('NFKD', self.odoo_client), unicodedata.normalize('NFKD', self.patient_name), self.created_at)
+
+class PhysicalExploration(models.Model):
+    time = models.DateTimeField("Dia/Hora",blank= True, null =True)
+    heart_rate = models.CharField("Frequencia Cardiaca", max_length=20,default='', blank=True)
+    respiratory_rate = models.CharField("Frequencia Respiratoria", max_length=20,default='', blank=True)
+    blood_pressure = models.CharField("Presion Arterial", max_length=20,default='', blank=True)
+    temperature = models.CharField("Temperature", max_length=20,default='', blank=True)
+    oxygen_saturation = models.CharField("Saturacion O2", max_length=20,default='', blank=True)
+    glucometry = models.CharField("Glucometria", max_length=20,default='', blank=True)
+    glassgow_motor = models.CharField("Glassgow Motor", max_length=20,default='', blank=True)
+    glassgow_verbal = models.CharField("Glassgow Verbal", max_length=20,default='', blank=True)
+    glassgow_ocular = models.CharField("Glassgow Ocular", max_length=20,default='', blank=True)
+    glassgow_final = models.CharField("Glassgow Final", max_length=20,default='', blank=True)
+
+    created_at = models.DateTimeField("Fecha de alta",auto_now_add=True,editable=False)
+    last_modified = models.DateTimeField("Última modificación",auto_now=True,editable=False)
+
+    class Meta:
+        verbose_name_plural = "Exploracion Fisica"
+        ordering = ['created_at']
+
+    def __str__(self):
+        return "{}, {}, {}, {}".format(unicodedata.normalize('NFKD',str(self.id)), self.created_at)
+
+
+#class Medications(models.Model):
 
