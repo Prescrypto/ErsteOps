25558d7874a3c1cc9a279e4ed36a093799086488
Juan Manuel Rivero <juanmanuelriverom@gmail.com>
2022-08-03 20:33:03 -0500

    fix logic for vidauno

--- ersteops/printpdf/views.py
+++ ersteops/printpdf/views.py
@@ -18,22 +18,31 @@ import tempfile
 import os
 import ast
 import unicodedata
-from petfile.views import get_petfile_json
+#from petfile.views import get_petfile_json
 from django.conf import settings
 import os.path
+
+from paperless.dict_fields import MedicalReportDict
+from paperless.models import MedicalReport
+
+# Logging library
+import logging
+logger = logging.getLogger('django_info')
+
 
 # Create your views here.
 # generate pdf for screen
 def document_as_pdf(request,pk):
   # Get document data
   user = request.user
-  params = get_petfile_json(request, pk)
-  print params
+  params = get_medical_report(pk)
+
+  print(params)
   # Generate QR Signature
   #generate_qr(signature)
   # Clean params strings
   params = escape_params(params)
-  params['diagnostic'] = (params['diagnostic'][:250]+ '...') if len(params['diagnostic']) > 250 else params['diagnostic']
+  #params['diagnostic'] = (params['diagnostic'][:250]+ '...') if len(params['diagnostic']) > 250 else params['diagnostic']
   # print "***************** diagnostic "
   # print params['diagnostic']
   # print "***************** diagnostic end "
@@ -43,27 +52,29 @@ def document_as_pdf(request,pk):
   tex_file = settings.BASE_DIR+'/templates/printpdf/rendered_template'
 
   if generate_pdf(tempdir,tex_file):    
-    print "PDF Generated Ok"
+    #print("PDF Generated Ok")
+    logger.info('[ PRINTPDF! - PDF Generated OK]')
     pdf_file = open(os.path.join(tempdir, 'rendered_template.pdf'), 'rb')
     pdf = pdf_file.read()
     r = HttpResponse(content_type='application/pdf')
     r.write(pdf)
     return r
   else:
-    print "PDF Error!!! ckeck /templates/printpdf/rendered_template.log" 
+    #print("PDF Error!!! ckeck /templates/printpdf/rendered_template.log")
+    logger.info('[ PRINTPDF! - PDF Error!!! ckeck /templates/printpdf/rendered_template.log]') 
     messages.error(request, "PDF Error!!! ckeck /templates/printpdf/rendered_template.log")
     return redirect('/petfile/review/')
 
 def document_as_pdf_print(request,pk):
   # Get document data
   user = request.user
-  params = get_petfile_json(request, pk)
-  print params
+  #params = get_petfile_json(request, pk)
+  print(params)
   # Generate QR Signature
   #generate_qr(signature)
   # Clean params strings
   params = escape_params(params)
-  params['diagnostic'] = (params['diagnostic'][:250]+ '...') if len(params['diagnostic']) > 250 else params['diagnostic']
+  #params['diagnostic'] = (params['diagnostic'][:250]+ '...') if len(params['diagnostic']) > 250 else params['diagnostic']
   # print "***************** diagnostic "
   # print params['diagnostic']
   # print "***************** diagnostic end "
@@ -73,14 +84,15 @@ def document_as_pdf_print(request,pk):
   tex_file = settings.BASE_DIR+'/templates/printpdf/rendered_template'
 
   if generate_pdf(tempdir,tex_file):    
-    print "PDF Generated Ok"
+    print("PDF Generated Ok")
     pdf_file = open(os.path.join(tempdir, 'rendered_template.pdf'), 'rb')
     pdf = pdf_file.read()
     r = HttpResponse(content_type='application/pdf')
     r.write(pdf)
     return r
   else:
-    print "PDF Error!!! ckeck /templates/printpdf/rendered_template.log" 
+    #print("PDF Error!!! ckeck /templates/printpdf/rendered_template.log" )
+    logger.info('[ PRINTPDF! - PDF Error!!! ckeck /templates/printpdf/rendered_template.log]') 
     messages.error(request, "PDF Error!!! ckeck /templates/printpdf/rendered_template.log")
     return redirect('/petfile/review/')
 
@@ -108,34 +120,41 @@ def generate_tex(params,output_mode):
   latex_overpic_head = '\\begin{overpic}[width=13.5cm,height=7cm,unit=1mm]{'+ settings.BASE_DIR +'/templates/printpdf/transparent_head.png}'
   latex_overpic_foot = '\\begin{overpic}[width=13.5cm,height=5cm,unit=1mm]{'+ settings.BASE_DIR +'/templates/printpdf/transparent_foot.png}'
   latex_graphics_path = '\\graphicspath{ {'+ settings.BASE_DIR +'/static/img/printpdf/} }'
-  medications_tex = generate_medication_tex(params)
+  #medications_tex = generate_medication_tex(params)
   # Generate context
   context = {
            'content': params,
            #'latex_image_path_local': latex_image_path_local,
            #'all_medications': params["petfile_medications"],
-           'all_medications': medications_tex,
+           #'all_medications': medications_tex,
            'latex_hospital_logo':latex_hospital_logo,
            'latex_overpic_head':latex_overpic_head,
            'latex_overpic_foot':latex_overpic_foot,
            'latex_graphics_path':latex_graphics_path,
        }
+  logger.info('[ PRINTPDF! - context {}]'.format(context)) 
   # Render template
   rendered_tpl = template.render(context).encode('utf-8')
   # Write Template to file
   try:
-    tex_file = open(settings.BASE_DIR+'/templates/printpdf/rendered_template.tex','w')
+    tex_file = open(settings.BASE_DIR+'/templates/printpdf/rendered_template.tex','wb')
     tex_file.write(rendered_tpl)
     tex_file.close()
+    logger.info('[ PRINTPDF! - generate_tex try OK]'.format(context)) 
     return True
-  except:  
+  except Exception as e:
+    logger.error('[ PRINTPDF! - generate_tex ERROR]')
+    logger.error(e)
+
+    return True
     return False
 
 def escape_params(params):
   escaped_params = {}
   for k,v in params.items():
     #print k, 'Corresponds to ', v
-    if isinstance(v,(str, unicode)):    
+    #if isinstance(v,(str, unicode)):
+    if isinstance(v,str):    
       escaped_params[k] = escape_text(v)
     else:
       escaped_params[k] = v 
@@ -161,15 +180,25 @@ def generate_pdf(tempdir,tex_file):
     os.remove(tex_file+'.pdf')
     os.remove(tex_file+'.aux')
     os.remove(tex_file+'.log')
-    print "Deleted pdf, log and tex file"
+
+    #print("Deleted pdf, log and tex file")
+    logger.info('[ PRINTPDF! - Deleted pdf, log and tex file]')
   except:
-    print "pdf, log and tex file dont exist!"
-  process = Popen(
-      ['pdflatex', '-output-directory', tempdir],
-      stdin=PIPE,
-      stdout=PIPE,
-  )
-  process.communicate(tex_file+'.tex')
+    #print("pdf, log and tex file dont exist!")
+    logger.info('[ PRINTPDF! - pdf, log and tex file dont exist!]')
+  try:  
+    process = Popen(
+        ['pdflatex', '-output-directory', tempdir],
+        stdin=PIPE,
+        stdout=PIPE,
+    )
+    tex_to_pdf = '{}.tex'.format(tex_file).encode()
+    process.communicate(tex_to_pdf)
+    logger.info('[ PRINTPDF! - tex to pdf succesfull]')
+  except Exception as e:
+    logger.error('[ PRINTPDF! - generate_tex ERROR 2]')
+    logger.error(e) 
+
   if os.path.exists(tex_file+'.pdf'):
     return True
   else:
@@ -212,5 +241,14 @@ def escape_text(s):
         s="NA"
     return s 
 
+
+
+
+def get_medical_report(pk):
+  #medical_Report = MedicalReport.get(id=pk)
+  medical_Report = MedicalReport.objects.filter(id=pk).values()[0]
+  
+
+  return medical_Report
 
 
